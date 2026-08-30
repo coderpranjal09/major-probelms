c93434462141ccbb43b8fb1d719a48fa012e23bb
Pranjal Maithani <Pranjalmaithani81822@gmail.com>
2026-08-30 19:08:40 +0530

    Create minAbsoluteDifference.java

---
+++ minAbsoluteDifference.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+    
+        Arrays.sort(arr);
+
+        List<List<Integer>> list = new ArrayList<>();
+        int minDiff = Integer.MAX_VALUE;
+        for(int i=0;i<arr.length-1;i++){
+           int diff = arr[i+1]-arr[i];
+
+           if(diff<minDiff){
+            minDiff =diff;
+            list.clear();
+            List<Integer> temp= new ArrayList<>();
+            temp.add(arr[i]);
+            temp.add(arr[i+1]);
+            list.add(temp);
+           }
+
+           else if(diff==minDiff){
+            List<Integer> temp= new ArrayList<>();
+            temp.add(arr[i]);
+            temp.add(arr[i+1]);
+            list.add(temp);
+           }
+        }
+
+        return list;
+    }
+}
